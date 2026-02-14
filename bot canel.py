8b9c6db55e599ffe19db3d25e87258dbdcd13095
phoenix_programming_group <amirgg0111111@gmail.com>
2026-02-14 21:33:35 +0330

    Add files via upload

---
+++ bot canel.py
@@ -0,0 +1,18 @@
+import telebot
+import os
+
+TOKEN = os.environ.get("8251517410:AAF3NWJTNKxMObs2dhWA4CEsBUfSIYgh3W0")
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, "سلام 👋 نظرتو بفرست")
+
+@bot.message_handler(func=lambda message: True)
+def get_message(message):
+    admin_id = 1086459356   # آیدی عددی خودت
+    bot.send_message(admin_id, f"پیام جدید:\n{message.text}")
+    bot.reply_to(message, "✅ ارسال شد")
+
+bot.infinity_polling()
